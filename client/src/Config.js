9c2290de573f45c0e4bb1caa57e50e149a22db38
cobbygraves <rmeocnis@hotmail.com>
2022-05-19 17:13:31 +0100

    added image preview

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,4 +1,4 @@
-// const HOSTURL = "https://myspacesfun.herokuapp.com";
-const HOSTURL = "http://localhost:5000";
+const HOSTURL = "https://myspacesfun.herokuapp.com";
+// const HOSTURL = "http://localhost:5000";
 
 export default HOSTURL;
